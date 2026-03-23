17d44e20d6403f501945cb2804bbf1cc6d319cbb
Odeyale Gbenga <gbengha2016@gmail.com>
2026-03-23 11:12:55 +0200

    Add test endpoint to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('test/', test_view),
 ]
