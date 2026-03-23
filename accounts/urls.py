f606cf5e319668393a1a8184cb1757a847011667
Odeyale Gbenga <gbengha2016@gmail.com>
2026-03-23 14:13:16 +0200

    Add test endpoint and verify accounts URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
 
+def test_accounts(request):
+    return HttpResponse("Accounts app is working!")
+
 urlpatterns = [
+    path('test/', test_accounts),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-
 ]
